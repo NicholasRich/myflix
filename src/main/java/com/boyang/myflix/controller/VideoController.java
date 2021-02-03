3374b40b0b8480e7e707762a33e28fa12977279a
Boyang <845657927@qq.com>
2021-02-03 05:07:48 +0000

    init

--- src/main/java/com/boyang/myflix/controller/VideoController.java
+++ src/main/java/com/boyang/myflix/controller/VideoController.java
@@ -3,6 +3,7 @@ package com.boyang.myflix.controller;
 import com.boyang.myflix.bean.Video;
 import com.boyang.myflix.service.VideoService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -10,6 +11,7 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.List;
 
 @RestController
+@CrossOrigin
 @RequestMapping("/video")
 public class VideoController {
     @Autowired
